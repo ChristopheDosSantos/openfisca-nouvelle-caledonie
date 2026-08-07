ee32d0197b8cbfdbf0a4a2038e3cfc5b090cd618
Christophe Dos Santos <christophe.dos-santos@gouv.nc>
2026-08-07 14:04:57 +1100

    feat(handicap):nb beneficiaires devant êter employés

--- openfisca_nouvelle_caledonie/variables/contribution_insertion_handicap/entreprise.py
+++ openfisca_nouvelle_caledonie/variables/contribution_insertion_handicap/entreprise.py
@@ -19,5 +19,33 @@ class soumis_obligation_emploi_handicapes(Variable):
     reference = "https://code.travail.gouv.fr/code-du-travail/L5212-1"
 
     def formula(entreprise, period, parameters):
-        effectif = entreprise('effectif_salarie', period)
-        return effectif > 20
+        effectif = entreprise("effectif_salarie", period)
+        seuil = (
+            parameters(period)
+            .contribution_insertion_handicap.obligation_emploi.seuil_effectif
+        )
+        return effectif > seuil
+
+
+class nb_beneficiaires_devant_etre_employes(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = YEAR
+    label = "Nombre de bénéficiaires que l'entreprise doit employer"
+
+    def formula(entreprise, period, parameters):
+        effectif = entreprise("effectif_salarie", period)
+        params = parameters(period).contribution_insertion_handicap.obligation_emploi
+
+        seuil = params.seuil_effectif
+        taux = params.taux_beneficiaires
+        minimum = params.minimum_fraction
+
+        valeur = effectif * taux
+        valeur_avec_minimum = where(
+            (valeur >= minimum) & (valeur < 1),
+            minimum,
+            valeur // 1,
+        )
+
+        return where(effectif > seuil, valeur_avec_minimum, 0)
